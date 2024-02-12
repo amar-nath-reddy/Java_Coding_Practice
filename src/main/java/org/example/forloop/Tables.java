c327959cdc6363b84843ced220158421f44581ba
amarn <amar2ra.vi@gmail.com>
2024-02-12 23:12:54 +0530

    2 table with for loop

---
+++ src/main/java/org/example/forloop/Tables.java
@@ -0,0 +1,13 @@
+package org.example.forloop;
+
+public class Tables {
+    //Print the Tables
+    public static void main(String[] args) {
+        System.out.println("Its 2 Table:");
+        System.out.println("============");
+        for(int i = 0 ; i <= 10 ; i++){
+            System.out.println("2"+" " +"*" +" "+i + "=" +" "+2*i );
+        }
+    }
+
+}
